f7193f8175daef8f50945d53edb6c8bc49f541fe
Jordan McLeod <jordandmcleod@outlook.com>
2022-10-16 15:58:11 +0100

    updating jquery

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-$(document).ready(function(){
-        var scroll_pos = 0;
-        $(document).scroll(function() {
-            scroll_pos = $(this).scrollTop();
-            if(scroll_pos > 100) {
-                $(".navigation-bar").addClass("navbar-scroll")
-            } else {
-              $(".navigation-bar").removeClass("navbar-scroll")
-            }
-        });
-    });
+$(document).scroll(function() {
+  var scroll_pos = 0;
+  scroll_pos = $(this).scrollTop();
+  if (scroll_pos > 75) {
+    $(".navigation-bar").addClass("navbar-scroll")
+  } else {
+    $(".navigation-bar").removeClass("navbar-scroll")
+  }
+});
